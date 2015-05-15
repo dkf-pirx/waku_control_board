956f25f14c6c7f6edc0300fde9fb3b070eaca6c2
Łukasz Niestoruk <niester@wp.pl>
2015-05-15 20:17:15 +0200

    Added new files for IO operations. Moved SetOutputs to io.h

---
+++ PCFanControl_WaCo/source/io.h
@@ -0,0 +1,7 @@
+#ifndef _IO_H
+#define _IO_H
+
+extern void SetOutputs( unsigned char pOutputs[3] );
+
+
+#endif // _IO_H
--- PCFanControl_WaCo/source/main.c
+++ PCFanControl_WaCo/source/main.c
@@ -3,49 +3,12 @@
  *
  */
 
-#define LATCH_UP     P2OUT |= BIT5
-#define LATCH_DOWN   P2OUT &= ~BIT5
-
     
 
 #include  "msp430g2553.h"
 #include  "support.h"
+#include  "io.h"
 
-void SetOutputs( unsigned char pOutputs[3] )
-{
-   int index; 
-   int value;
-
-   // prepare to take output
-   LATCH_DOWN;
-   
-   for ( index = 0; index < 8; ++index )
-   {
-      // set serial in value
-      value = 0x80 & pOutputs[0];
-      
-      if ( value == 0 ) // clear
-      {
-         P2OUT &= ~BIT3;
-      }
-      else // set
-      {
-         P2OUT |= BIT3;
-      }
-      
-      // rising edge of the clock
-      P2OUT |= BIT4;
-
-      // falling edge of the clock
-      P2OUT &= ~BIT4;
-      
-      // shift to next serial in
-      pOutputs[0] <<= 1;
-   }
-   
-   // take over output
-   LATCH_UP;
-}
 
 void main(void)
 {
